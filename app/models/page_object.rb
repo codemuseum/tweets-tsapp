58fcd3fab5195c36450b5ba46d24f2f4bd57544c
Matthew Moore <matt@thrivesmart.com>
2009-03-10 15:50:18 -0600

    css tweaks

--- app/models/page_object.rb
+++ app/models/page_object.rb
@@ -10,6 +10,12 @@ class PageObject < ActiveRecord::Base
   self.caching_default = 'interval[60]' 
   #[in :forever, :page_object_update, :any_page_object_update, 'data_update[datetimes]', :never, 'interval[5]']
 
+  def after_intialize
+    if new_record?
+      self.number_of_updates = DEFAULT_NUMBER_OF_UPDATES
+    end
+  end
+
   def tweetless?
     username.blank? || tweets.nil? || tweets.empty?
   end
@@ -17,7 +23,8 @@ class PageObject < ActiveRecord::Base
   # Calls twitter for tweets correctly, and returns a JSON object, which is simply a hash of strings
   def tweets
     return @tweets unless @tweets.nil?
-    url = "#{BASE_URL}/#{user}?count=#{number_of_updates || DEFAULT_NUMBER_OF_UPDATES}"
+    return [] if username.blank?
+    url = "#{BASE_URL}/#{username}.json?count=#{number_of_updates || DEFAULT_NUMBER_OF_UPDATES}"
     @tweets = JSON.parse(open(url).read)
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,4 +15,4 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
-config.action_controller.asset_host                  = "209.20.86.103"
+config.action_controller.asset_host                  = "localhost:3001"
